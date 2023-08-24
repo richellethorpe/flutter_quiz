7067828e08f69289c3287caed5f056837fd189a7
Richelle <richelle.ak@gmail.com>
2023-08-24 11:05:54 -0700

    add background color to box decoration on main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quiz/start_screen.dart';
+
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: StartScreen(),
+  runApp( MaterialApp(
+    home: Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 98, 196, 46),
+          Color.fromARGB(255, 196, 237, 174),
+        ]),),
+        child: const StartScreen(),
       ),
-    )
-  );
+    ),
+  ));
 }
