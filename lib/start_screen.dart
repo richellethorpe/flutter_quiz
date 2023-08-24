ebfd4a3e0b05db722a4f9eb812b273e3c658349e
Richelle <richelle.ak@gmail.com>
2023-08-24 10:59:24 -0700

    add basic file structure and startscreen

---
+++ lib/start_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return const Text('Start Screen');
+  }
+}
