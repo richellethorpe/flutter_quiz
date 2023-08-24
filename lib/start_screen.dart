b80da28a6a2f935128917b1e6ad2cdf30c9f9a10
Richelle <richelle.ak@gmail.com>
2023-08-24 11:38:24 -0700

    add formated button to homepage

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -17,10 +17,16 @@ class StartScreen extends StatelessWidget {
           const SizedBox(height: 80),
           const Text(
             'Learn Flutter the fun way!',
-            style: TextStyle(
-                color: Colors.white, 
-                fontSize: 24),
+            style: TextStyle(color: Colors.white, fontSize: 24),
           ),
+          const SizedBox(height: 30),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            child: const Text('Start Quiz'),
+          )
         ],
       ),
     );
