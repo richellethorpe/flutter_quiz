be05c82e981befc219088d2812fb6d02d89a6066
Richelle <richelle.ak@gmail.com>
2023-08-24 13:33:09 -0700

    add switchScreen function

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -2,7 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(context) {
@@ -23,7 +25,7 @@ class StartScreen extends StatelessWidget {
           const SizedBox(height: 30),
           OutlinedButton.icon(
             onPressed: () {
-              
+              startQuiz();
             },
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
