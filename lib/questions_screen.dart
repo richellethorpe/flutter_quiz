a4b8ff3c9b6f570cb8e729c5d11af056919cf707
Richelle <richelle.ak@gmail.com>
2023-08-27 12:38:14 -0700

    add mapping to dynamically display question and answers

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quiz/answer_button.dart';
+import 'package:flutter_quiz/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -13,29 +14,24 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
     return SizedBox(
       //double.infinity means use the entire width
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'The question...',
-            style: TextStyle(color: Color.fromARGB(255, 23, 23, 23)),
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(color: Color.fromARGB(255, 23, 23, 23)),
           ),
           const SizedBox(height: 30),
-          AnswerButton(
-            answerText: 'answer 1',
-            onTap: () {},
-          ),
-           AnswerButton(
-            answerText: 'answer 2',
-            onTap: () {},
-          ),
-          AnswerButton(
-            answerText: 'answer 3',
-            onTap: () {},
-          ),
+          ...currentQuestion.answers.map(
+            (element) => AnswerButton(
+              answerText: element,
+              onTap: () {},
+            ),
+          )
         ],
       ),
     );
